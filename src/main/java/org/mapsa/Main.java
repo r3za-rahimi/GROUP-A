ae7d77fc5eec08e652a7086b0c884214d9ef0db9
r3zar <rrahimi25@gmail.com>
2023-02-04 11:29:08 +0330

    MASTER

--- src/main/java/org/mapsa/Main.java
+++ src/main/java/org/mapsa/Main.java
@@ -1,7 +1,12 @@
-package org.example;
+package org.mapsa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("GROUP A!");
+
+
+
     }
 }
